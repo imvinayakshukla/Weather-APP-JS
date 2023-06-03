65c8de2a518902dda57f3c5441bc1c87654cff4d
Vinayak Shukla <vinayakshukla0786@gmail.com>
2023-06-03 10:14:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ async function checkWeather(city)
     var data=await response.json();
     console.log(data);
     if(data.weather[0].main=="Clouds")
-    weather_icon.src="/images/clouds.png";
+    weather_icon.src="./images/clouds.png";
     else if(data.weather[0].main=="Clear")
-    weather_icon.src="/images/clear.png";
+    weather_icon.src="./images/clear.png";
 
 
     else if(data.weather[0].main=="Rain")
-    weather_icon.src="images/rain.png";
+    weather_icon.src="./images/rain.png";
     else if(data.weather[0].main=="Drizzle")
-    weather_icon.src="images/drizzle.png";
+    weather_icon.src="./images/drizzle.png";
     else if(data.weather[0].main=="Mist")
-    weather_icon.src="images/mist.png";
+    weather_icon.src="./images/mist.png";
 
 
     document.querySelector(".city").innerHTML=data.name;
